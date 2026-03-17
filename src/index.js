1054ad07752bf57a555125f7c154431b12d65f0c
LaBren90 <101529095+LaBren90@users.noreply.github.com>
2026-03-17 12:51:19 +0000

    updated interface from poem project to fact project

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ function generatePoem(event) {
 
   let promptInput = document.querySelector("#user-prompt");
   let apiKey = "a9f2b6a7f83210eb03od0ab344b8t656";
-  let prompt = `User instructions: Generate a poem about ${promptInput.value}`;
+  let prompt = `User instructions: Generate a fact about ${promptInput.value}`;
   let context =
-    "You are a poet who is great at making up 4-line poems from a user prompt. Be polite and follow the user instructions to create your poem. Use basic HTML and display the poem with each line beginning on a new line on the page. do not include any other titles or comments. do not show html tag";
+    "you are an ai assistant who knows many facts about countries of the world. use the country name given by the user to generate a short and interesting fact. Do no include any title.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="animate-flicker">⏳Generating your poem about ${promptInput.value}...</div>`;
+  poemElement.innerHTML = `<div class="animate-flicker">⏳Generating your fact about ${promptInput.value}...</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
