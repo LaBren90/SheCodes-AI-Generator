de5791daf4fd65d331526af92c5f1acb6a96254e
LaBren90 <101529095+LaBren90@users.noreply.github.com>
2026-03-17 13:04:50 +0000

    updated classes and ids on elements from previous project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
+function displayFact(response) {
+  new Typewriter("#fact", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -7,21 +7,21 @@ function displayPoem(response) {
   });
 }
 
-function generatePoem(event) {
+function generateFact(event) {
   event.preventDefault();
 
   let promptInput = document.querySelector("#user-prompt");
   let apiKey = "a9f2b6a7f83210eb03od0ab344b8t656";
   let prompt = `User instructions: Generate a fact about ${promptInput.value}`;
   let context =
-    "you are an ai assistant who knows many facts about countries of the world. use the country name given by the user to generate a short and interesting fact. Do no include any title.";
+    "you are an ai assistant who knows many facts about countries of the world. use the country name given by the user to generate a short and interesting fact. Do no include any title. include an emoji that relates to the fact";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="animate-flicker">⏳Generating your fact about ${promptInput.value}...</div>`;
+  let FactElement = document.querySelector("#fact");
+  FactElement.classList.remove("hidden");
+  FactElement.innerHTML = `<div class="animate-flicker">⏳Generating your fact about ${promptInput.value}...</div>`;
 
-  axios.get(apiURL).then(displayPoem);
+  axios.get(apiURL).then(displayFact);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let FactFormElement = document.querySelector("#fact-generator-form");
+FactFormElement.addEventListener("submit", generateFact);
